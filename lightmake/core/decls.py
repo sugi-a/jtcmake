1b0235da62ec3d5e995885dba0c60e2484ef8a92
sugia <42127588+sugi-a@users.noreply.github.com>
2022-05-08 06:15:43 +0900

    delete unnecessary class definition

--- lightmake/core/decls.py
+++ lightmake/core/decls.py
@@ -2,15 +2,6 @@ from abc import ABCMeta, abstractmethod
 
 NOP = object()
 
-class ITarget(metaclass=ABCMeta):
-    @abstractmethod
-    def path(self):
-        pass
-
-    @abstractmethod
-    def _get_rule(self):
-        pass
-
 
 class Rule:
     def __init__(self, name, method, args, kwargs, depset, self_path_set, dep_paths):
@@ -21,5 +12,4 @@ class Rule:
         self.depset = depset
         self.self_path_set = self_path_set
         self.dep_path_set = dep_paths
-
 
