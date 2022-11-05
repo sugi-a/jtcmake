46ea54040b4c516bb1cc6e142470c0d4c731ca6d
sugia <42127588+sugi-a@users.noreply.github.com>
2022-11-05 11:39:32 +0900

    Add SELF to jtcmake.__init__

--- src/jtcmake/__init__.py
+++ src/jtcmake/__init__.py
@@ -6,7 +6,7 @@ from .group_tree.groups import (
 )
 from .group_tree.core import make
 from .group_tree.atom import Atom
-from .group_tree.rule import Rule
+from .group_tree.rule import Rule, SELF
 from .rule.file import File, VFile
 from .group_tree.graphviz import print_graphviz
 from .group_tree.misc import print_method
@@ -15,6 +15,7 @@ from .core.make import MakeSummary
 VERSION = "0.3.0"
 
 __all__ = [
+    "SELF",
     "UntypedGroup",
     "StaticGroupBase",
     "GroupOfRules",
