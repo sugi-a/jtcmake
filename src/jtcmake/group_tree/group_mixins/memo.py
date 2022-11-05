39290747bcde89a07c1c23159c091fd90d1a0816
sugia <42127588+sugi-a@users.noreply.github.com>
2022-11-05 11:34:33 +0900

    Add test and refactor

--- src/jtcmake/group_tree/group_mixins/memo.py
+++ src/jtcmake/group_tree/group_mixins/memo.py
@@ -1,17 +1,17 @@
 from typing import TypeVar
-from ..core import IGroup
+from ..core import INode
 from ..atom import Atom
 
 T = TypeVar("T")
 
 
-class MemoMixin(IGroup):
-    def mem(self, value: T, memoized_value: object) -> T:
+class MemoMixin(INode):
+    def mem(self: INode, value: T, memoized_value: object) -> T:
         self._get_info().memo_store[id(value)] = Atom(value, memoized_value)
         return value
 
-    def memstr(self, value: T) -> T:
-        return self.mem(value, str(value))
+    def memstr(self: INode, value: T) -> T:
+        return MemoMixin.mem(self, value, str(value))
 
-    def memnone(self, value: T) -> T:
-        return self.mem(value, None)
+    def memnone(self: INode, value: T) -> T:
+        return MemoMixin.mem(self, value, None)
---
+++ tests/group_tree/group_mixins/test_memo.py
@@ -0,0 +1,21 @@
+from jtcmake.group_tree.group_mixins.memo import MemoMixin
+from jtcmake.group_tree.groups import StaticGroupBase
+
+
+def test_MemoMixin():
+    a, b, c, d = [object() for _ in range(4)]
+
+    g = StaticGroupBase()
+
+    MemoMixin.mem(g, a, b)
+    MemoMixin.memstr(g, c)
+    MemoMixin.memnone(g, d)
+
+    store = g._get_info().memo_store  # pyright: ignore [reportPrivateUsage]
+
+    assert store[id(a)].value == a
+    assert store[id(a)].memo_value == b
+    assert store[id(c)].value == c
+    assert store[id(c)].memo_value == str(c)
+    assert store[id(d)].value == d
+    assert store[id(d)].memo_value == None
